3bbd2e850f8da673b2124e776b411bb87c3680a2
Efrain1096 <Efrainmiguel@live.com>
2025-06-15 17:05:18 -0700

    New scripts for creating the nodes, so to speak.

---
+++ Assets/CreateDungeon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDungeon: MonoBehaviour
+{
+
+    public int mapWidth = 50; //X direction
+    public int mapDepth = 50; //Z direction
+    public int scale = 2;
+    Leaf root;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        root = new Leaf(0, 0, mapWidth, mapDepth, scale);
+        //root.Draw();
+
+
+        int leftWidth = Random.Range(10, 20);
+
+        Leaf left = new Leaf(0, 0, mapWidth/2, mapDepth, scale);
+        Leaf right = new Leaf(mapWidth/2, 0, mapWidth/2, mapDepth, scale);
+        left.Draw();
+        right.Draw();
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Leaf.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : MonoBehaviour
+{
+
+    int xPos;
+    int zPos;
+    int width;
+    int depth;
+    int scale;
+
+    public Leaf(int x, int z, int w, int d, int s) // The constructor for the leaf nodes.
+    {
+        xPos = x;
+        zPos = z;
+        width = w;
+        depth = d;
+        scale = s;
+
+    }
+
+
+    public void Draw()
+    {
+
+        Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+
+        for (int x = xPos; x < width + xPos; x++)
+        {
+            for (int z = zPos; z < depth + zPos; z++) // Create 3D cubes of 1 unit. 
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(x * scale, 0, z * scale); // Muliply the scale by the cube's position to separate them apart.
+                cube.transform.localScale = new Vector3(scale, scale, scale);
+                cube.GetComponent<Renderer>().material.SetColor("_Color", c);
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+}
