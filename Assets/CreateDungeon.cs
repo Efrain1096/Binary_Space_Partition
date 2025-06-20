8c5de55e60b188e4d2f836395c7705642de4268b
Efrain1096 <Efrainmiguel@live.com>
2025-06-19 20:40:10 -0700

    I messed up by importing the final Unity project file from the tutorial... So... now it's done, lol. I just wanted to see how the final result would look like in trying to fix a bug I had. But it ended up overwriting the scripts completely without being able to undo the changes. I also made the mistake in doing a lot of code changes without doing a commit. Let this be a harsh reminder of why frequent changes should be uploaded.

--- Assets/CreateDungeon.cs
+++ Assets/CreateDungeon.cs
@@ -4,51 +4,153 @@ using UnityEngine;
 
 public class CreateDungeon : MonoBehaviour
 {
-
-    public int mapWidth = 50; //X direction
-    public int mapDepth = 50; //Z direction
+    public int mapWidth = 50;
+    public int mapDepth = 50;
     public int scale = 2;
     Leaf root;
+
+    byte[,] map;
+    List<Vector2> corridors = new List<Vector2>();
 
     // Start is called before the first frame update
     void Start()
     {
+        map = new byte[mapWidth, mapDepth];
+        for (int z = 0; z < mapDepth; z++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                map[x, z] = 1;
+            }
+        }
 
         root = new Leaf(0, 0, mapWidth, mapDepth, scale);
-        BSP(root, 20);
+        BSP(root, 6); // The second argument is how many "levels" or how deep the tree will be.
 
+        AddCorridors();
+        AddRandomCorridors(10);
+
+        DrawMap();
     }
 
-    void BSP(Leaf l, int splitDepth)
+    void AddRandomCorridors(int numHalls)
     {
-
-        if (l == null) { return; }
-
-        if (splitDepth <= 0)
+        for (int i = 0; i < numHalls; i++)
         {
-            l.Draw(0);
+            int startX = Random.Range(5, mapWidth - 5);
+            int startZ = Random.Range(5, mapDepth - 5);
+            int length = Random.Range(5, mapWidth - 5);
+
+            if (Random.Range(0, 100) < 50)
+            { line(startX, startZ, length, startZ); }
+            else
+            { line(startX, startZ, startX, length); }
+        }
+    }
+
+    void BSP(Leaf leaf, int sDepth)
+    {
+        if (leaf == null) { return; }
+
+        if (sDepth <= 0)
+        {
+            leaf.Draw(map);
+            corridors.Add(new Vector2(leaf.xPos + leaf.width / 2, leaf.zPos + leaf.depth / 2)); //This will get the center of the leaf, or room.
             return;
         }
 
-        if (l.Split())
+        if (leaf.Split())
         {
-            BSP(l.leftChild, splitDepth - 1);
-            BSP(l.rightChild, splitDepth - 1);
+            BSP(leaf.leftChild, sDepth - 1); //Recursion at its finest!
+            BSP(leaf.rightChild, sDepth - 1);
         }
         else
         {
-            l.Draw(0);
+            leaf.Draw(map);
+            corridors.Add(new Vector2(leaf.xPos + leaf.width / 2, leaf.zPos + leaf.depth / 2));
         }
-
     }
 
-    void Update()
+    void AddCorridors()
     {
+        for (int i = 1; i < corridors.Count; i++)
+        {
+            if ((int)corridors[i].x == (int)corridors[i - 1].x || (int)corridors[i].y == (int)corridors[i - 1].y)
+            {
+                line((int)corridors[i].x, (int)corridors[i].y, (int)corridors[i - 1].x, (int)corridors[i - 1].y);
+            }
+            else
+            {
+                line((int)corridors[i].x, (int)corridors[i].y, (int)corridors[i].x, (int)corridors[i - 1].y);
+                line((int)corridors[i].x, (int)corridors[i].y, (int)corridors[i - 1].x, (int)corridors[i].y);
+            }
+        }
+    }
 
+    void DrawMap()
+    {
+        for (int z = 0; z < mapDepth; z++)
+            for (int x = 0; x < mapWidth; x++)
+            {
+                if (map[x, z] == 1) // Create the blocks that are used for the walls.
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x * scale, 10, z * scale);
+                    cube.transform.localScale = new Vector3(scale, scale, scale);
+                }
+                else if (map[x, z] == 2) // Create and color the blocks that represent the corridors.
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x * scale, 10, z * scale);
+                    cube.transform.localScale = new Vector3(scale, scale, scale);
+                    cube.GetComponent<Renderer>().material.SetColor("_Color", new Color(1, 0, 0));
+                }
+            }
+    }
+
+
+    public void line(int x1, int y1, int x2, int y2) //An adapted version of Bresenham's line algorithm for blocks instead of pixels.
+    {
+        int w = x2 - x1;
+        int h = y2 - y1;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+
+        if (w < 0) { dx1 = -1; }
+        else if (w > 0) { dx1 = 1; }
+
+        if (h < 0) { dy1 = -1; }
+        else if (h > 0) { dy1 = 1; }
+
+        if (w < 0) { dx2 = -1; }
+        else if (w > 0) { dx2 = 1; }
+
+        int longest = Mathf.Abs(w);
+        int shortest = Mathf.Abs(h);
+        if (!(longest > shortest))
+        {
+            longest = Mathf.Abs(h);
+            shortest = Mathf.Abs(w);
+            if (h < 0) { dy2 = -1; }
+            else if (h > 0) { dy2 = 1; }
+
+            dx2 = 0;
+        }
+        int numerator = longest >> 1;
+        for (int i = 0; i <= longest; i++)
+        {
+            map[x1, y1] = 0;
+            numerator += shortest;
+            if (!(numerator < longest))
+            {
+                numerator -= longest;
+                x1 += dx1;
+                y1 += dy1;
+            }
+            else
+            {
+                x1 += dx2;
+                y1 += dy2;
+            }
+        }
     }
 }
-
-
-
-
-
--- Assets/Leaf.cs
+++ Assets/Leaf.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class Leaf
 {
-
-    int xPos;
-    int zPos;
-    int width;
-    int depth;
+    public int xPos;
+    public int zPos;
+    public int width;
+    public int depth;
     int scale;
     int roomMin = 5;
 
     public Leaf leftChild;
     public Leaf rightChild;
-    public Leaf(int x, int z, int w, int d, int s) // The constructor for the leaf nodes.
+
+    public Leaf(int x, int z, int w, int d, int s)
     {
         xPos = x;
         zPos = z;
@@ -26,56 +25,51 @@ public class Leaf
 
     public bool Split()
     {
-
-        if (width <= roomMin || depth <= roomMin) { return false; } // This will restrict our sections or "rooms" not be tiny and useless.
+        if (width <= roomMin || depth <= roomMin) { return false; }
 
         bool splitHorizontal = Random.Range(0, 100) > 50;
-
-        if (width > depth && width / depth >= 1.2) { splitHorizontal = false; }
-        else if (depth > width && depth / width >= 1.2) { splitHorizontal = true; }
-
+        
+        if (width > depth && width / depth >= 1.2)
+        { splitHorizontal = false; }
+        else if (depth > width && depth / width >= 1.2)
+        { splitHorizontal = true; }
 
         int max = (splitHorizontal ? depth : width) - roomMin;
-
         if (max <= roomMin) { return false; }
 
         if (splitHorizontal)
         {
-            int leftDepth = Random.Range(roomMin, max);
-            leftChild = new Leaf(xPos, zPos, width, leftDepth, scale);
-            rightChild = new Leaf(xPos, zPos + leftDepth, width, depth - leftDepth, scale);
+            int l1depth = Random.Range(roomMin, max);
+            leftChild = new Leaf(xPos, zPos, width, l1depth, scale);
+            rightChild = new Leaf(xPos, zPos + l1depth, width, depth - l1depth, scale);
         }
         else
         {
-            int leftWidth = Random.Range(roomMin, max);
-            leftChild = new Leaf(xPos, zPos, leftWidth, depth, scale);
-            rightChild = new Leaf(xPos + leftWidth, zPos, width - leftWidth, depth, scale);
-
-
+            int l1width = Random.Range(roomMin, max);
+            leftChild = new Leaf(xPos, zPos, l1width, depth, scale);
+            rightChild = new Leaf(xPos + l1width, zPos, width - l1width, depth, scale);
         }
+
         return true;
-
-
-        /*  int leftWidth = Random.Range( (int)(mapDepth * 0.1f), (int)(mapDepth * 0.7f));
-            Leaf left = new Leaf(0, 0, leftWidth, mapDepth, scale);
-            Leaf right = new Leaf(leftWidth, 0, mapWidth - leftWidth , mapDepth, scale);
-            left.Draw();
-            right.Draw();
-            */
     }
 
-    public void Draw(int level)
+    public void Draw(byte[,] map)
     {
-        Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+        /*Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
         for (int x = xPos; x < width + xPos; x++)
-        {
-            for (int z = zPos; z < depth + zPos; z++) // Create 3D cubes of 1 unit. 
+            for (int z = zPos; z < depth + zPos; z++)
             {
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(x * scale, level * 20, z * scale); // Muliply the scale by the cube's position to separate them apart.
+                cube.transform.position = new Vector3(x * scale, 0, z * scale);
                 cube.transform.localScale = new Vector3(scale, scale, scale);
-                cube.GetComponent<Renderer>().material.SetColor("_Color", c); // Add some color to distiguish the different partitions.
+                cube.GetComponent<Renderer>().material.SetColor("_Color", c);
+            }*/
+
+        int wallSize = Random.Range(1, 3);
+        for (int x = xPos + wallSize; x < width + xPos - wallSize; x++)
+            for (int z = zPos + wallSize; z < depth + zPos - wallSize; z++)
+            {
+                map[x, z] = 0; //"Write" the empty sections to the map of the dungeon.
             }
-        }
     }
 }
